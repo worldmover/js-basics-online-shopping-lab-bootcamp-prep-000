e9be70be1e0e648df0648f1d6c998ab01e03b4a1
Christian Weiss <worldmover777@gmail.com>
2018-02-02 06:00:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -83,13 +83,15 @@ function removeFromCart(item) {
  } 
 
 function placeOrder(cardNumber) {
+   if(cardNumber===undefined){
+    console.log("Sorry, we don't have a credit card on file for you.")
+  }
+  
   if(cardNumber!=undefined){
     console.log(`Your total cost is $${total()}, which will be 
     charged to the card ${cardNumber}.`)
     cart.splice(0,cart.length)
   }
-  if(cardNumber===undefined){
-    console.log("Sorry, we don't have a credit card on file for you.")
-  }
+ 
   }
 
