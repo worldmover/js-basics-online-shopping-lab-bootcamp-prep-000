98660f4ae29152458edeb3d6a2d6ca573361aee8
Christian Weiss <worldmover777@gmail.com>
2018-02-02 05:58:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ function removeFromCart(item) {
   // return cart
   //   }
   }
-  console.log("That item is not in your cart")
+  console.log("That item is not in your cart.")
   return cart
  } 
 
