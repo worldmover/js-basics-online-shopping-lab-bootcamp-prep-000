26d056176e3882d5e6cf54d58a4a416b812dfaa6
Christian Weiss <worldmover777@gmail.com>
2018-02-02 05:30:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if(cardNumber!=undefined){
-    console.log(`Your total cost is ${total()}, which will be 
+    console.log(`Your total cost is $${total()}, which will be 
     charged to the card ${cardNumber}.`)
     cart.splice(0,cart.length)
   }
