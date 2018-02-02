12be50b935cacf61c51d36a319c439efd004d5e0
Christian Weiss <worldmover777@gmail.com>
2018-02-02 06:01:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ function removeFromCart(item) {
  } 
 
 function placeOrder(cardNumber) {
-   if(cardNumber===undefined){
+   if(!cardNumber){
     console.log("Sorry, we don't have a credit card on file for you.")
   }
   
