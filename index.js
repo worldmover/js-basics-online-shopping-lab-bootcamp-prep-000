c5c1b677d443dded063166d8eb3aab314de181a8
Christian Weiss <worldmover777@gmail.com>
2018-02-02 05:33:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -90,6 +90,7 @@ function placeOrder(cardNumber) {
     console.log(`Your total cost is $${total()}, which will be 
     charged to the card ${cardNumber}.`)
     cart.splice(0,cart.length)
+    return cart
   }
   if(cardNumber===undefined){
     console.log("Sorry, we don't have a credit card on file for you.")
